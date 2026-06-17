c83b2a861af0119376fdd6a8068207fe83f3fc4d
Saurabh kushwaha <saurabh2323401@gmail.com>
2026-06-17 21:15:00 +0530

    Create MinStack.cpp

---
+++ Week_3/Day_3/MinStack.cpp
@@ -0,0 +1,24 @@
+class MinStack {
+public:
+    stack<int> s, minS;
+
+    MinStack() {}
+
+    void push(int val) {
+        s.push(val);
+        if (minS.empty() || val <= minS.top()) minS.push(val);
+    }
+    
+    void pop() {
+        if (s.top() == minS.top()) minS.pop();
+        s.pop();
+    }
+    
+    int top() {
+        return s.top();
+    }
+    
+    int getMin() {
+        return minS.top();
+    }
+};
