9e9a7fae20f40b7e95181b1f181ec5d363c5e5cd
Saurabh kushwaha <saurabh2323401@gmail.com>
2026-06-23 21:10:20 +0530

    Create BinaryTreeInorderTraversal.cpp

---
+++ Week_4/Day_2/BinaryTreeInorderTraversal.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        helper(root, res);
+        return res;
+    }
+    
+    void helper(TreeNode* root, vector<int>& res) {
+        if (!root) return;
+        helper(root->left, res);
+        res.push_back(root->val);
+        helper(root->right, res);
+    }
+};
